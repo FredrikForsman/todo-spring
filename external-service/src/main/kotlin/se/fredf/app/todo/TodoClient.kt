659a0a807d8521dec27f96c6c9ed9a6e480e06f6
Fredrik Forsman <fredf@redpill-linpro.com>
2020-02-06 08:30:09 +0100

    Better naming for todo and api gateway and deleted controller from eureka server

--- external-service/src/main/kotlin/se/fredf/app/todo/TodoClient.kt
+++ external-service/src/main/kotlin/se/fredf/app/todo/TodoClient.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient("TodoClient")
+@FeignClient("todo-service")
 interface TodoClient {
 
     @GetMapping("/todos/get")
